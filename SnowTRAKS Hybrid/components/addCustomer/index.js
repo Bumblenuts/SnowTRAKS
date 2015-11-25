8a8629cf0e41a29f292d0b49f71654c1849a89ae
Dave Branson <d.branson@fieldtraks.ca>
2015-11-25 17:54:58 +0000

    Changes from Screen Builder

---
+++ SnowTRAKS Hybrid/components/addCustomer/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+app.addCustomer = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_addCustomer
+// END_CUSTOM_CODE_addCustomer
+(function(parent) {
+    var addCustomerModel = kendo.observable({
+        fields: {
+            email: '',
+            mobile: '',
+            homeTel: '',
+            streetAddress: '',
+            customerName: '',
+        },
+        submit: function() {},
+        cancel: function() {}
+    });
+
+    parent.set('addCustomerModel', addCustomerModel);
+})(app.addCustomer);
+
+// START_CUSTOM_CODE_addCustomerModel
+// END_CUSTOM_CODE_addCustomerModel
